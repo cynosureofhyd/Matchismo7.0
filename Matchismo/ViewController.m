f0f347b61260f830e2e0112a44733ee5db1469d0
Cynosure <cynosure@Cynosures-MacBook-Pro.local>
2013-12-08 16:55:02 -0700

    CardMatchingGame

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -4,20 +4,30 @@
 //
 //  Created by Cynosure on 12/6/13.
 //  Copyright (c) 2013 prashanth. All rights reserved.
-//
-
 #import "ViewController.h"
 #import "Deck.h"
+#import "CardMatchingGame.h"
 #import "PlayingCardDeck.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *flipLabel;
 @property (nonatomic) int flipCount;
-@property   (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchingGame *game;
+
 @end
 
 @implementation ViewController
 
+-(CardMatchingGame *)game
+{
+    if (!_game) {
+        _game = [[CardMatchingGame alloc]initWithCardCount:0 usingDeck:[self createDeck]];
+    }
+    return _game;
+}
 
 -(Deck *)deck
 {
@@ -30,26 +40,26 @@
     return [[PlayingCardDeck alloc]init];
 }
 
+-(void)setCardButtons:(NSArray *)cardButtons
+{
+    _cardButtons = cardButtons;
+    for(UIButton *cardButton in cardButtons)
+    {
+        Card *card = [self.deck drawRandomCard];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+    }
+}
+
 -(void)setFlipCount:(int)flipCount
 {
     _flipCount = flipCount;
     self.flipLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
 
-
 - (IBAction)flipButton:(UIButton *)sender {
-    if(sender.isSelected)
-        sender.selected = NO;
-    else
-    {
-        Card *randomCard = [self.deck drawRandomCard];
-        if(randomCard)
-        {
-            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
-            sender.selected = YES;
-        }
-    }
-     self.flipCount++;
+    sender.selected = !sender.selected;
+    
+    self.flipCount++;
 }
 
 @end
